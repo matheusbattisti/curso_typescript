e39663bba4b5edf243fc55949c9fd5cec7db6ddb
Matheus Battisti <matheusbudkewicz@gmail.com>
2021-11-10 18:02:55 -0200

    First commit! =)

---
+++ 1_INTRO/index.js
@@ -0,0 +1,9 @@
+var firstName = 'Matheus';
+var anotherName = 1;
+var x = true;
+function greeting(name) {
+    console.log('Olá, ' + name);
+}
+greeting(firstName);
+greeting(anotherName);
+greeting(x);
---
+++ 1_INTRO/index.ts
@@ -0,0 +1,11 @@
+const firstName = 'Matheus'
+const anotherName = 1
+const x = true
+
+function greeting(name: string) {
+  console.log('Olá, ' + name)
+}
+
+greeting(firstName)
+// greeting(anotherName)
+// greeting(x)
