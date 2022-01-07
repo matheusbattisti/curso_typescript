724c67ba25cdec9493a693518c0a5c022a3066fb
Matheus Battisti <matheusbudkewicz@gmail.com>
2022-01-07 14:13:39 -0300

    finalizando projeto

--- 14_ts_api_restful/src/controllers/movieController.ts
+++ 14_ts_api_restful/src/controllers/movieController.ts
@@ -6,9 +6,6 @@ import { MovieModel } from "../models/Movie";
 // Logger
 import Logger from "../../config/logger";
 
-// Validator
-import { validationResult } from "express-validator";
-
 export async function createMovie(req: Request, res: Response) {
   try {
     const data = req.body;
@@ -55,6 +52,24 @@ export async function removeMovie(req: Request, res: Response) {
     await movie.delete();
 
     return res.status(201).json({ msg: "Filme removido com sucesso!" });
+  } catch (e: any) {
+    Logger.info(`Erro no sistema: ${e.message}`);
+  }
+}
+
+export async function updateMovie(req: Request, res: Response) {
+  try {
+    const id = req.params.id;
+    const data = req.body
+    const movie = await MovieModel.findById(id);
+
+    if (!movie) {
+      return res.status(404).json({ error: "O filme não existe." });
+    }
+
+    await MovieModel.updateOne({_id: id}, data);
+
+    return res.status(200).json(data);
   } catch (e: any) {
     Logger.info(`Erro no sistema: ${e.message}`);
   }
--- 14_ts_api_restful/src/router.ts
+++ 14_ts_api_restful/src/router.ts
@@ -4,6 +4,7 @@ import {
   findMovieById,
   getAllMovies,
   removeMovie,
+  updateMovie,
 } from "./controllers/movieController";
 
 // validations
@@ -19,4 +20,5 @@ export default router
   .post("/movie", movieCreateValidation(), validate, createMovie)
   .get("/movie/:id", findMovieById)
   .get("/movie", getAllMovies)
-  .delete("/movie/:id", removeMovie);
+  .delete("/movie/:id", removeMovie)
+  .patch("/movie/:id", movieCreateValidation(), validate, updateMovie);
